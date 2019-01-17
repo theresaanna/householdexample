a7cc5c2bbc4ebe84b0ddf9376b7bbf42b7ab4fec
Theresa Summa <theresasumma@Theresas-MacBook-Air.local>
2019-01-17 12:33:50 -0800

    Deal with errors semi-properly

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-// your code goes here ...
+// To be unit tested
 function verifyAge(age) {
   age = parseInt(age, 10);
   const ageCheck = age !== NaN && age > 0 ? true : false;
   return ageCheck;
 }
 
+// To be unit tested
 function verifyExists(attr) {
-  const attrCheck = attr === "" ? false : true;
+  const attrCheck = attr === "" || attr === undefined ? false : true;
   return attrCheck;
 }
 
 const householdContainer = document.getElementsByClassName("household")[0];
 const addButton = document.getElementsByClassName("add")[0];
 const submitButton = document.querySelector("button[type=submit]");
+const pageContainer = document.getElementsByClassName("builder")[0];
+let errorsContainer = document.createElement('div');
+errorsContainer.id = "errors";
+pageContainer.appendChild(errorsContainer);
 
 function renderHouseholdMember(values) {
-  let memberHTML = "<ul>";
+  let memberHTML = "";
   for (var key in values) {
     memberHTML += "<li>" + key + ": " + values[key] + "</li>";
   }
-  memberHTML += "</ul>";
-  householdContainer.innerHTML = memberHTML;
+  householdContainer.innerHTML += memberHTML;
 }
 
 function renderError(validAge, hasRelationship) {
   const errors = [
-    "Please verify that you have input a valid age and try again.",
-    "Please select a relationship and try again."
+    "Please verify that you have input a valid age and try again.<br/>",
+    "Please select a relationship and try again.<br/>"
   ];
+  errorsContainer.innerHTML = "";
   for (var i = 0; i <= 1; i++) {
     if (arguments[i] === false) {
-      householdContainer.innerHTML = errors[i];
+        errorsContainer.innerHTML += errors[i];
     }
   }
 }
@@ -38,6 +43,7 @@ function renderError(validAge, hasRelationship) {
 addButton.addEventListener('click', function(e){
   e.preventDefault();
   let formValues = {};
+
   for (var i = 0; i < document.forms[0].length; i++) {
     if (document.forms[0][i].value && document.forms[0][i].value !== "") {
       formValues[document.forms[0][i].name] = document.forms[0][i].value;
